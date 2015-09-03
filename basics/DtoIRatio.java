5d1ff31eb5f9946097fee291c3792470629ade3b
Alicia McNett <alicia.mcnett@gmail.com>
2015-09-03 09:57:49 -0400

    Updated In-class Example for Thursday
    Added some comments

--- basics/DtoIRatio.java
+++ basics/DtoIRatio.java
@@ -5,10 +5,18 @@ public class DToIRatio
 {
 	public static void main(String[] args)
 	{
-		int ratio = 0;		
+		int ratio = 0;
 
-		//debt-to-income ratio is debt/income multiplied by 100
+		// step 1
+		// debt per month is 1200
+		// income per month is 2300
+		// declare and initalize variables needed
+
+		// step 2
+		// debt-to-income ratio is debt/income multiplied by 100, no decimal places please!
+		// put equation below and store result in ratio
 
 		System.out.println("The debt-to-income ratio is: " + ratio + "%");
+		// note: anything over 43% is not very good
 	}
 }
