66b97afb430926e93f2c0685312d0ed8a11f66b4
Alicia McNett <alicia.mcnett@gmail.com>
2015-10-09 10:00:26 -0400

    Getting ready for chapter 4
    new files!

---
+++ selection/Triangle.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;  // Needed for the Scanner class
+
+/**
+ * Date: October 2015
+ * Purpose: This program correctly identifies the type of triangle given the side lengths. 
+ */
+
+public class Triangle
+{
+   public static void main(String[] args)
+   {
+      int side1, side2, side3; // Three sides
+
+      // Create a Scanner object to read input.
+      Scanner keyboard = new Scanner(System.in);
+
+      // Get a side1.
+      System.out.print("Enter first side: ");
+      side1 = keyboard.nextLine();
+      
+      // Get a side2
+      System.out.print("Enter second side: ");
+      side2 = keyboard.nextLine();
+
+      // Get a side3
+      System.out.print("Enter third side: ");
+      side3 = keyboard.nextLine();
+
+      //test to see if it is a valid triangle
+	  //if it is valid, what kind of triangle is it?
+   }
+}
