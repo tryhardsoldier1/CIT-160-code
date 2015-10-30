974e19d68baf6337576b130c0a21717540cba253
Alicia McNett <alicia.mcnett@gmail.com>
2015-10-30 09:14:40 -0400

    Loop stuff & new files for FileIO
    Starter file for File IO

---
+++ loops/ContinueDemo.java
@@ -0,0 +1,22 @@
+// Demo from Oracle.com documentation
+// Site: https://docs.oracle.com/javase/tutorial/java/nutsandbolts/branch.html
+// The continue statement skips the current iteration of a for, while , or do-while loop.
+
+class ContinueDemo {
+    public static void main(String[] args) {
+
+        String searchMe = "peter piper picked a " + "peck of pickled peppers";
+        int max = searchMe.length();
+        int numPs = 0;
+
+        for (int i = 0; i < max; i++) {
+            // interested only in p's
+            if (searchMe.charAt(i) != 'p')
+                continue;
+
+            // process p's
+            numPs++;
+        }
+        System.out.println("Found " + numPs + " p's in the string.");
+    }
+}
--- loops/GraderLoop/src/Grader.java
+++ loops/GraderLoop/src/Grader.java
@@ -9,12 +9,22 @@ import java.util.Scanner;
 
 public class Grader {
   public static void main(String[] args){
-	
+
     Scanner keyboard = new Scanner(System.in);
     System.out.println("Please enter a grade or -1 to quit");
     int grade = keyboard.nextInt();
-	
+    int count = 0;
+    int sum = 0;
     double average = 0;
+
+    while(grade != -1){
+      sum += grade;
+      count++;
+      System.out.println("Please enter a grade or -1 to quit");
+      grade = keyboard.nextInt();
+    }
+
+    average = ((double)sum)/count;
     int roundedAverage = (int)round(average);
     System.out.println("The average is " + roundedAverage);
   }
